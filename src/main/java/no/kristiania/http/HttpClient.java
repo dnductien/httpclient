0613519b31869f95cbc7a64fdb3bb79eabc3f3c1
ductien.dangnguyen <ductien.dangnguyen@gmail.com>
2021-09-06 14:24:03 +0200

    updated final project

--- src/main/java/no/kristiania/http/HttpClient.java
+++ src/main/java/no/kristiania/http/HttpClient.java
@@ -1,11 +1,66 @@
 package no.kristiania.http;
 
-public class HttpClient {
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.Socket;
+import java.util.HashMap;
 
-    public HttpClient(String s, int i, String s1) {
+public class HttpClient {
+    private final int statusCode;
+    private final HashMap<String, String> headerFields = new HashMap<>();
+    private String messageBody;
+    public HttpClient(String host, int port, String requestTarget) throws IOException {
+        Socket socket = new Socket(host, port);
+        socket.getOutputStream().write(("GET " + requestTarget + " HTTP/1.1\r\nHost: " + host + "\r\n\r\n").getBytes());
+
+        String[] statusLine = readLine(socket.getInputStream()).split(" ");
+        statusCode = Integer.parseInt(statusLine[1]);
+
+        String headerField;
+        while (!(headerField = readLine(socket.getInputStream())).isBlank()) {
+            int colonPos = headerField.indexOf(':');
+            headerFields.put(headerField.substring(0, colonPos), headerField.substring(colonPos+1).trim());
+        }
+        this.messageBody = readBytes(socket.getInputStream(), getContentLength());
+    }
+
+    private String readBytes(InputStream in, int contentLength) throws IOException {
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < contentLength; i++) {
+            result.append((char)in.read());
+        }
+        return result.toString();
+    }
+
+    private String readLine(InputStream in) throws IOException {
+        StringBuilder result = new StringBuilder();
+        int c;
+        while ((c = in.read()) != -1 && c != '\r') {
+            result.append((char)c);
+        }
+        if (c == '\r') {
+            in.read();
+        }
+        return result.toString();
+    }
+
+    public String getHeader(String fieldName) {
+        return headerFields.get(fieldName);
+    }
+
+    public int getContentLength() {
+        return Integer.parseInt(getHeader("Content-Length"));
+    }
+
+    public String getMessageBody() {
+        return messageBody;
     }
 
     public int getStatusCode() {
-        return 200;
+        return statusCode;
+    }
+    public static void main(String[] args) throws IOException {
+        HttpClient client = new HttpClient("httpbin.org", 80, "/html");
+        System.out.println(client.getMessageBody());
     }
 }
--- src/test/java/no/kristiania/http/HttpClientTest.java
+++ src/test/java/no/kristiania/http/HttpClientTest.java
@@ -2,12 +2,35 @@ package no.kristiania.http;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class HttpClientTest {
     @Test
-    void shouldGetResponseCode() {
+    void shouldGetResponseCode() throws IOException {
         HttpClient client = new HttpClient("httpbin.org", 80, "/status/200");
         assertEquals(200, client.getStatusCode());
+    }
+
+    @Test
+    void shouldReadHeaders() throws IOException {
+        HttpClient client = new HttpClient("httpbin.org", 80, "/html");
+        assertEquals("text/html; charset=utf-8", client.getHeader("Content-Type"));
+    }
+
+    @Test
+    void shouldReadContentLength() throws IOException {
+        HttpClient client = new HttpClient("httpbin.org", 80, "/html");
+        assertTrue(client.getContentLength() > 10);
+    }
+
+    @Test
+    void shouldReadMessageBody() throws IOException {
+        HttpClient client = new HttpClient("httpbin.org", 80, "/html");
+        assertTrue(client.getMessageBody().startsWith("<!DOCTYPE html>\n<html>"),
+                "expected <" + client.getMessageBody() + " to be html"
+        );
     }
 }
